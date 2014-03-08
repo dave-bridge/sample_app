caed0e57fcdd2580382f8031c5c0b82580402e23
Bridgester <david.bridge.13@aberdeen.ac.uk>
2014-03-08 23:16:50 +0000

    Initial commit

---
+++ vendor/bundle/ruby/2.0.0/gems/nokogiri-1.6.1/ext/nokogiri/siteconf20140308-59443-18l9rga.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140308-59443-r2i7di"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
---
+++ vendor/bundle/ruby/2.0.0/gems/sqlite3-1.3.8/ext/sqlite3/siteconf20140308-59443-14q02jh.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140308-59443-a1ia75"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
